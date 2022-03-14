9a1612783d748cc5ef0b0a05a0a2985d4dfdeea7
luizzlcs <luizzlcs@hotmail.com>
2022-03-13 22:52:40 -0300

    Assignmento operator + equals e hashcode

--- lib/14_assignmento_operators/1_assignmento_operators.dart
+++ lib/14_assignmento_operators/1_assignmento_operators.dart
@@ -1,9 +1,24 @@
+String? nome;
 void main() {
+  // Operadores de atribuição
   // =  -=  /=  %= >>= ^=
 // +=  *=  ~/=  <<=  &=  |=
 
-  var numero = 1;
-
-  numero = numero + 2;
+  var numero = 7;
+  // Numero é = numero + 2
+  numero += 2;
   print(numero);
+
+  // numero = número menos 2
+  numero -= 2;
+  print(numero);
+  var numeros2 = 6.0;
+  // Numeros2 recebe o valor definido
+  // anteriormente e divide por 2;
+  numeros2 /= 2;
+  print(numeros2);
+
+  // Null Operator, se a variavel for nula retorna 'Luiz Carlos';
+  nome ??= 'Luiz Carlos';
+  print(nome);
 }
